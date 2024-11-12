ecf86d383891263951d24dbdc7e18c105653e05c
RayenEsen <rayentriki2003@gmail.com>
2024-11-12 16:15:02 +0100

    Responsivity  fixes

--- footer.php
+++ footer.php
@@ -12,24 +12,36 @@
       </div>
       <p class="styled-text">Thank you for visiting our site. We strive to offer the best services and innovative solutions to meet your needs.</p>
       <div class="flex items-center gap-x-4 mt-12" style="margin-top: 1rem;">
-        <button aria-label="instagram" class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 opacity-50 w-8 h-8 flex-shrink-0 bg-gray-800 cursor-pointer hover:bg-gray-700 rounded-full flex items-center justify-center">
-           <svg xmlns="http://www.w3.org/2000/svg"  width="27" height="27" viewBox="0,0,270,250"
-          style="fill:#FFFFFF;">
-          <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(8.53333,8.53333)"><path d="M15,2c-7.168,0 -13,5.832 -13,13c0,7.168 5.832,13 13,13c7.168,0 13,-5.832 13,-13c0,-7.168 -5.832,-13 -13,-13zM11.66602,6h6.66602c3.125,0 5.66797,2.54202 5.66797,5.66602v6.66602c0,3.125 -2.54202,5.66797 -5.66602,5.66797h-6.66602c-3.125,0 -5.66797,-2.54202 -5.66797,-5.66602v-6.66602c0,-3.125 2.54202,-5.66797 5.66602,-5.66797zM11.66602,8c-2.021,0 -3.66602,1.64597 -3.66602,3.66797v6.66602c0,2.021 1.64597,3.66602 3.66797,3.66602h6.66602c2.021,0 3.66602,-1.64597 3.66602,-3.66797v-6.66601c0,-2.021 -1.64597,-3.66602 -3.66797,-3.66602zM19.66797,9.66602c0.368,0 0.66602,0.29802 0.66602,0.66602c0,0.368 -0.29801,0.66797 -0.66602,0.66797c-0.368,0 -0.66797,-0.29997 -0.66797,-0.66797c0,-0.368 0.29997,-0.66602 0.66797,-0.66602zM15,10c2.757,0 5,2.243 5,5c0,2.757 -2.243,5 -5,5c-2.757,0 -5,-2.243 -5,-5c0,-2.757 2.243,-5 5,-5zM15,12c-1.65685,0 -3,1.34315 -3,3c0,1.65685 1.34315,3 3,3c1.65685,0 3,-1.34315 3,-3c0,-1.65685 -1.34315,-3 -3,-3z"></path></g></g>
-          </svg>
-        </button>
-        <button aria-label="facebook" class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 opacity-50 w-8 h-8 flex-shrink-0 bg-gray-800 cursor-pointer hover:bg-gray-700 rounded-full flex items-center justify-center">
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <a href="https://www.instagram.com/mastersky_bmt?igsh=MTg1dXUyc21sd3M0cg%3D%3D" target="_blank" rel="noopener noreferrer">
+    <button aria-label="instagram" class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 opacity-50 w-8 h-8 flex-shrink-0 bg-gray-800 cursor-pointer hover:bg-gray-700 rounded-full flex items-center justify-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0,0,270,250" style="fill:#FFFFFF;">
+            <g fill="#ffffff" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+                <g transform="scale(8.53333,8.53333)">
+                    <path d="M15,2c-7.168,0 -13,5.832 -13,13c0,7.168 5.832,13 13,13c7.168,0 13,-5.832 13,-13c0,-7.168 -5.832,-13 -13,-13zM11.66602,6h6.66602c3.125,0 5.66797,2.54202 5.66797,5.66602v6.66602c0,3.125 -2.54202,5.66797 -5.66602,5.66797h-6.66602c-3.125,0 -5.66797,-2.54202 -5.66797,-5.66602v-6.66602c0,-3.125 2.54202,-5.66797 5.66602,-5.66797zM11.66602,8c-2.021,0 -3.66602,1.64597 -3.66602,3.66797v6.66602c0,2.021 1.64597,3.66602 3.66797,3.66602h6.66602c2.021,0 3.66602,-1.64597 3.66602,-3.66797v-6.66601c0,-2.021 -1.64597,-3.66602 -3.66797,-3.66602zM19.66797,9.66602c0.368,0 0.66602,0.29802 0.66602,0.66602c0,0.368 -0.29801,0.66797 -0.66602,0.66797c-0.368,0 -0.66797,-0.29997 -0.66797,-0.66797c0,-0.368 0.29997,-0.66602 0.66797,-0.66602zM15,10c2.757,0 5,2.243 5,5c0,2.757 -2.243,5 -5,5c-2.757,0 -5,-2.243 -5,-5c0,-2.757 2.243,-5 5,-5zM15,12c-1.65685,0 -3,1.34315 -3,3c0,1.65685 1.34315,3 3,3c1.65685,0 3,-1.34315 3,-3c0,-1.65685 -1.34315,-3 -3,-3z"></path>
+                </g>
+            </g>
+        </svg>
+    </button>
+</a>
+
+<a href="https://www.facebook.com/profile.php?id=61565778207998" target="_blank" rel="noopener noreferrer">
+    <button aria-label="facebook" class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 opacity-50 w-8 h-8 flex-shrink-0 bg-gray-800 cursor-pointer hover:bg-gray-700 rounded-full flex items-center justify-center">
+        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="white"/>
             <path d="M15.58 8.27h-2.13V6.79c0-.4.34-.74.74-.74h1.39V3.84H13.3c-1.51 0-2.73 1.23-2.73 2.73v1.7H8.95v2.43h1.62v6.22h2.52v-6.22h2.13l.36-2.43Z" fill="gray"/>
-          </svg>
-        </button>
-        <button aria-label="linkedin" class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 opacity-50 w-8 h-8 flex-shrink-0 bg-gray-800 cursor-pointer hover:bg-gray-700 rounded-full flex items-center justify-center">
-          <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="12" cy="12" r="10" fill="white" />
-            <path d="M10 16.5H8V10h2v6.5zm-1-7.4c-.5 0-.9-.4-.9-.9s.4-.9.9-.9.9.4.9.9-.4.9-.9.9zm7.5 7.4h-2V13c0-.7-.3-1.2-.9-1.2s-.9.5-.9 1.2v3.5h-2V10h2v1c.4-.6 1-.9 1.6-.9 1.2 0 2.2.8 2.2 2.6v4.7z" fill="#0A66C2" />
-          </svg>
-        </button>
+        </svg>
+    </button>
+</a>
+
+<a href="https://www.tiktok.com/@mastersky49?_t=8rJVxTZxCeu&_r=1" target="_blank" rel="noopener noreferrer">
+    <button style=" background-color: rgb(179 179 179);"  aria-label="tiktok" class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 w-8 h-8 flex-shrink-0 bg-gray-800 cursor-pointer hover:bg-gray-700 rounded-full flex items-center justify-center">
+        <svg width="20" height="20" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle  cx="24" cy="24" r="24" fill="white"/>
+            <path d="M31.5 12.5c.5 0 1.5.5 2 1.5 1 1 2.5 2 4 2v3c-2 0-3-1-4-2-1-.5-1.5-1.5-2-1.5V29c0 3.5-3 6-7 6s-7-2.5-7-6c0-3.5 3-6 7-6v3c-2.5 0-4 1.5-4 3s1.5 3 4 3 4-1.5 4-3V12.5h3z" fill="gray"/>
+        </svg>
+    </button>
+</a>
+
 
 
       </div>
